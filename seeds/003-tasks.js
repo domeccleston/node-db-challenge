f8bf84cb3c75195b36aa88c05e91b62e92a098a8
Dom <domeccleston@gmail.com>
2019-11-08 11:37:22 +0000

    formatting boolean

--- seeds/003-tasks.js
+++ seeds/003-tasks.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('tasks').insert([
-        {id: 1, task_name: 'Finish full stack web curriculum', description: 'level up skills', completed: false, project_id: 1},
-        {id: 2, task_name: 'Complete push day today', description: 'get reps in', completed: true, project_id: 2},
+        {id: 1, task_name: 'Finish full stack web curriculum', description: 'level up skills', completed: 'false', project_id: 1},
+        {id: 2, task_name: 'Complete push day today', description: 'get reps in', completed: 'true', project_id: 2},
       ]);
     });
 };
